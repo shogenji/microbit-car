ed0c13d745964265f9cab12cabfd909cbc2fd32e
shogenji <ruirui@ruirui.jp>
2022-10-17 22:23:29 +0900

    更新 pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
 /** 
  * これはHoge（ホゲ）という何にも役に立たないクラスです
  */
-namespace Hoge { }
+namespace Hoge {
+    //% blockId=hoge
+    //% block="hoge %huga"
+    export function hoge(huga: number): void {
+        basic.showNumber(huga)
+    }
+}
